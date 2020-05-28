ec7eebb0ab6d3e811f6389bec8c1f4f66305058c
danielsun <danielsun12345@gmail.com>
2020-05-27 20:32:44 -0700

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,21 @@
 class CouponsController < ApplicationController
+    
+    def index
+        @coupon = Coupon.all
+    end 
 
+    def show 
+        @coupon = Coupon.find_by(params[:id])
+    end 
+
+    def new 
+    end 
+
+    def create
+        @coupon = Coupon.new
+        @coupon.store = params[:store]
+        @coupon.coupon_code = params[:coupon_code]
+        @coupon.save
+        redirect_to coupon_path(@coupon)
+    end 
 end 
