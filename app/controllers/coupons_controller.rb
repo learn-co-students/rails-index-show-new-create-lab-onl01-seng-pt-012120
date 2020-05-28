cfc18f46b2a84ab5f80718bbb036203518e6e55e
danielsun <danielsun12345@gmail.com>
2020-05-27 21:34:36 -0700

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,21 +1,22 @@
 class CouponsController < ApplicationController
-    
+
     def index
-        @coupon = Coupon.all
-    end 
-
-    def show 
-        @coupon = Coupon.find_by(params[:id])
-    end 
-
-    def new 
-    end 
-
-    def create
+        @coupons = Coupon.all
+      end
+    
+      def new
+      end
+    
+      def create
         @coupon = Coupon.new
-        @coupon.store = params[:store]
-        @coupon.coupon_code = params[:coupon_code]
+        @coupon.coupon_code = params[:coupon][:coupon_code]
+        @coupon.store = params[:coupon][:store]
         @coupon.save
+    
         redirect_to coupon_path(@coupon)
-    end 
+      end
+    
+      def show
+        @coupon = Coupon.find(params[:id])
+      end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :coupons, :only [:index, :new, :create]
-  get '/coupon/:id', to: 'coupons#show', as: 'coupon'
+  resources :coupons, only: [:index, :new, :create, :show]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
